f5e0981927fb60aa072285d8102fdecbb99b2ec8
Your Name <you@example.com>
2024-09-24 16:34:37 +0300

    server url change

--- src/fetch/api/DATA_SOURCE_URL.ts
+++ src/fetch/api/DATA_SOURCE_URL.ts
@@ -1,3 +1,4 @@
-export const DATA_SOURCE_URL = "https://hsr-back-1.onrender.com";
+export const DATA_SOURCE_URL = "https://hsr-back-2.onrender.com";
 //http://localhost:3000
 //https://hsr-back-1.onrender.com
+//https://hsr-back-2.onrender.com
