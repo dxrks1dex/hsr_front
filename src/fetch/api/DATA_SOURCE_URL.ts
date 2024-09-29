5c9176e0730e55cbd9c1963b655ba423ae39032a
Your Name <you@example.com>
2024-09-29 13:38:13 +0300

    fix timer icon view

--- src/fetch/api/DATA_SOURCE_URL.ts
+++ src/fetch/api/DATA_SOURCE_URL.ts
@@ -1,4 +1,5 @@
-export const DATA_SOURCE_URL = "https://hsr-back-2.onrender.com";
+export const DATA_SOURCE_URL = "https://hsr-back-3.onrender.com";
 //http://localhost:3000
 //https://hsr-back-1.onrender.com
 //https://hsr-back-2.onrender.com
+//https://hsr-back-3.onrender.com
