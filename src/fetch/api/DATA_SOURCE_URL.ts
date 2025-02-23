9d23f6c71ae9b3dfc568265c769a24e75939694f
Your Name <you@example.com>
2025-02-24 00:01:59 +0200

    data src link

--- src/fetch/api/DATA_SOURCE_URL.ts
+++ src/fetch/api/DATA_SOURCE_URL.ts
@@ -1,4 +1,4 @@
-export const DATA_SOURCE_URL = "http://localhost:3000";
+export const DATA_SOURCE_URL = "https://hsr-back-3.onrender.com";
 //http://localhost:3000
 //https://hsr-back-1.onrender.com
 //https://hsr-back-2.onrender.com
